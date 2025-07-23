3d68004bd79b3ee54f5ce17c5ca65caaf0ca34c9
Clemens Koza <clemens.koza@gmx.at>
2025-07-23 14:15:39 +0200

    update version and correct dependencies

--- version.php
+++ version.php
@@ -26,9 +26,12 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_modcontentservice';
 $plugin->release = '0.1.0';
-$plugin->version = 2025072100;
+$plugin->version = 2025072300;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
+    'mod_assign' => ANY_VERSION,
+    'mod_folder' => ANY_VERSION,
     'mod_page' => ANY_VERSION,
+    'mod_resource' => ANY_VERSION,
 ];
