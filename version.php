743ef74e4c4fd164ebdc644dc888d357059c2938
Clemens Koza <ckoza@tgm.ac.at>
2025-08-06 23:30:34 +0200

    add more functions useful for managing course modules to the service

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_modcontentservice';
 $plugin->release = '0.1.0';
-$plugin->version = 2025072300;
+$plugin->version = 2025080700;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
